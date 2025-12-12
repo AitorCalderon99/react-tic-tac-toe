99f862ecfee0d850ebd6156b0a21838fe08edd87
Aitor Calderon <aitorclzamakola@gmail.com>
2025-12-12 13:34:07 +0100

    feat(game): Add basic gameboard and functionallity

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import PlayerInfo from "./components/PlayerInfo/PlayerInfo.jsx";
+import GameBoard from "./components/GameBoard/GameBoard.jsx";
+import {useState} from "react";
 
 function App() {
-  
+  const [activePlayer, setActivePlayer] = useState('X');
 
-  return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+  function handleSelectSquare() {
+    setActivePlayer((currentPlayer) => currentPlayer === 'X' ? 'O' : 'X');
+  }
+
+  return (<main>
+    <div id="game-container">
+      <ol id="players" className="highlight-player">
+        <PlayerInfo name="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
+        <PlayerInfo name="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
+      </ol>
+      <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
+    </div>
+  </main>)
 }
 
 export default App
---
+++ src/components/PlayerInfo/PlayerInfo.jsx
@@ -0,0 +1,23 @@
+import {useState} from "react";
+
+export default function PlayerInfo({name, symbol, isActive}) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(name);
+
+  function toggleIsEditing() {
+    setIsEditing((editing) => !editing);
+  }
+
+  function onSetPlayerName(event) {
+    setPlayerName(event.target.value);
+  }
+
+  return (<li className={isActive && 'active'}>
+    <span className="player">
+      {isEditing ? (<input type="text" required value={playerName} onChange={onSetPlayerName}/>) :
+        (<span className="player-name">{playerName}</span>)}
+      <span className="player-symbol">{symbol}</span>
+    </span>
+    <button onClick={toggleIsEditing}>{isEditing ? 'Save' : 'Edit'}</button>
+  </li>)
+}
